cff3cbbc92db387b6851b1f6eb516583cf5f662f
Josue Nascimento <josuetk63@gmail.com>
2018-11-04 19:02:11 -0200

    Aplicando tecnicas de clean code

--- api.py
+++ api.py
@@ -149,18 +149,12 @@ def create_app():
             data.update({'$lte': request.args['data_final']})
         return data
 
-    def fill_cardapios_idade(dictionary, lista_cardapios, idades, refeicoes):
+    def preenche_cardapios_idade(lista_cardapios, idades):
         for dictionary in lista_cardapios:
             dictionary['idade'] = idades[dictionary['idade']]
         return dictionary
 
-    @app.route('/cardapios')
-    @app.route('/cardapios/<data>')
-    @swag_from('swagger_docs/cardapios.yml')
-    def get_cardapios(data=None):
-        query = {
-            'status': 'PUBLICADO'
-        }
+    def define_query_from_request(query):
         if request.args.get('agrupamento'):
             query['agrupamento'] = request.args['agrupamento']
         if request.args.get('tipo_atendimento'):
@@ -169,6 +163,34 @@ def create_app():
             query['tipo_unidade'] = request.args['tipo_unidade']
         if request.args.get('idade'):
             query['idade'] = idades_reversed.get(request.args['idade'])
+        return query
+
+    def definir_ordenacao(definicao_ordenacao, cardapios):
+        _cardapios = []
+        for cardapio in cardapios:
+            _cardapios.append(cardapio)
+        cardapio_ordenado =[]
+        for item in definicao_ordenacao:
+            for cardapio in _cardapios:
+                if item == cardapio['idade']:
+                    cardapio_ordenado.append(cardapio)
+                    continue
+        return cardapio_ordenado
+
+    def refeicoes_cardapio(cardapios,refeicoes):
+            for cardapio in cardapios:
+                for item in refeicoes:
+                    if refeicoes[item] in cardapios['cardapio']:
+                        cardapios['cardapio'][refeicoes[item]] = sorted(cardapios['cardapio']
+                                                                            [refeicoes[item]])
+    @app.route('/cardapios')
+    @app.route('/cardapios/<data>')
+    @swag_from('swagger_docs/cardapios.yml')
+    def get_cardapios(data=None):
+        query = {
+            'status': 'PUBLICADO'
+        }
+        query = define_query_from_request(query)
         query = fill_data_query(query, data, request)
         limit = int(request.args.get('limit', 0))
         page = int(request.args.get('page', 0))
@@ -179,7 +201,6 @@ def create_app():
         }
         cardapios = db.cardapios.find(query, fields).sort([('data', -1)])
         cardapios = cardapios_from_db(page, limit, cardapios)
-        _cardapios = []
         cardapio_ordenado = []
         definicao_ordenacao = ['A - 0 A 1 MES', 'B - 1 A 3 MESES',
                                'C - 4 A 5 MESES', 'D - 0 A 5 MESES',
@@ -197,25 +218,9 @@ def create_app():
                                'S - FILHOS PRO JOVEM', 'V - PROFESSOR',
                                'U - PROFESSOR JANTAR CEI']
 
-        for c in cardapios:
-            _cardapios.append(c)
-
-        for i in definicao_ordenacao:
-            for c in _cardapios:
-                if i == c['idade']:
-                    cardapio_ordenado.append(c)
-                    continue
-
-        for c in cardapio_ordenado:
-            c['idade'] = idades[c['idade']]
-
-        ##c = fill_cardapios_idade(c,cardapio_ordenado,idades,refeicoes)
-
-        for c in cardapio_ordenado:
-            for x in refeicoes:
-                if refeicoes[x] in c['cardapio']:
-                    c['cardapio'][refeicoes[x]] = sorted(c['cardapio']
-                                                          [refeicoes[x]])
+        cardapio_ordenado = definir_ordenacao(definicao_ordenacao, cardapios)
+        preenche_cardapios_idade(cardapio_ordenado,idades)
+        refeicoes_cardapio(cardapios, refeicoes)
 
         response = app.response_class(
             response=json_util.dumps(cardapio_ordenado),
